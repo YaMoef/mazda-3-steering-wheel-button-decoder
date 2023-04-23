d4704dc7cb7c189db16e88cf08330c1f57059f8f
YaMoef <brentclaessens@outlook.com>
2023-04-23 23:07:24 +0200

    FEAT: add simple button detection
    Simple button detection has been added to be able to see which
    buttons was pressed. There is a standard deviation to take in
    account tolerances. The buttons have to be tweaked to be detected
    correctly.

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+| Button               | Resistance (ohm) | arduino value |
+| -------------------- | ---------------- | ------------- |
+| VOL -                | 55               | 915           |
+| VOL +                | 148              | 860           |
+| AUTO SCAN            | 301.3            | 705           |
+| PRESET               | 563              | 625           |
+| MODE                 | 1.041k           | 475           |
+| MUTE                 | 2.040K           | 406           |
+| When nothing pressed | 5.02K            | 176           |
--- src/main.cpp
+++ src/main.cpp
@@ -2,36 +2,66 @@
 
 #define ANALOG_PIN A0
 
-const int LOWER_BARRIER = 10;
+const int STANDARD_DEVIATION = 3.5; // deviation in percent 1 ~= 10
 
-const int VOL_UP = 850; // 148 ohm
-const int VOL_DOWN = 1000; // 55 ohm
+const int VOL_DOWN = 915;
+const int VOL_DOWN_DELAY = 150;
 
-const int UP = 700; // 301.3 ohm
-const int DOWN = 500; // 563 ohm
+const int VOL_UP = 860;
+const int VOL_UP_DELAY = 150;
 
-const int MODE = 250; // 1.041 k
-const int MUTE = 10; // 2.040 k
+const int UP = 705;
+const int UP_DELAY = 150;
 
-void setup() {
+const int DOWN = 625;
+const int DOWN_DELAY = 150;
+
+const int MODE = 475;
+const int MODE_DELAY = 150;
+
+const int MUTE = 406;
+const int MUTE_DELAY = 150;
+
+void setup()
+{
+  Serial.begin(9600);
   // put your setup code here, to run once:
 }
 
-void loop() {
+void loop()
+{
   int value = analogRead(ANALOG_PIN);
-  if(value>LOWER_BARRIER){
+  // Serial.println(VOL_DOWN * ((100.0 - STANDARD_DEVIATION) / 100.0));
+  delay(10);
+  if (value >= VOL_DOWN * ((100.0 - STANDARD_DEVIATION) / 100.0) && value <= VOL_DOWN * ((100.0 + STANDARD_DEVIATION) / 100.0))
+  {
+    Serial.println("VOL DOWN: " + String(value) + "/" + VOL_DOWN);
 
+    delay(VOL_DOWN_DELAY);
   }
-  else if(){
-
+  else if (value >= VOL_UP * ((100.0 - STANDARD_DEVIATION) / 100.0) && value <= VOL_UP * ((100.0 + STANDARD_DEVIATION) / 100.0))
+  {
+    Serial.println("VOL_UP: " + String(value) + "/" + VOL_UP);
+    delay(VOL_UP_DELAY);
   }
-  else if(){
-
+  else if (value >= UP * ((100.0 - STANDARD_DEVIATION) / 100.0) && value <= UP * ((100.0 + STANDARD_DEVIATION) / 100.0))
+  {
+    Serial.println("UP: " + String(value) + "/" + UP);
+    delay(UP_DELAY);
   }
-  else if(){
-
+  else if (value >= DOWN * ((100.0 - STANDARD_DEVIATION) / 100.0) && value <= DOWN * ((100.0 + STANDARD_DEVIATION) / 100.0))
+  {
+    Serial.println("DOWN: " + String(value) + "/" + DOWN);
+    delay(DOWN_DELAY);
   }
-  else if(){
-    
+  else if (value >= MODE * ((100.0 - STANDARD_DEVIATION) / 100.0) && value <= MODE * ((100.0 + STANDARD_DEVIATION) / 100.0))
+  {
+    Serial.println("MODE: " + String(value) + "/" + MODE);
+    delay(MODE_DELAY);
+  }
+  else if (value >= MUTE * ((100.0 - STANDARD_DEVIATION) / 100.0) && value <= MUTE * ((100.0 + STANDARD_DEVIATION) / 100.0))
+  {
+    Serial.println("MUTE: " + String(value) + "/" + MUTE);
+    delay(MUTE_DELAY);
   }
 }
