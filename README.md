ab818923ae2115deda05b351e074807b539aba5d
YaMoef <brentclaessens@outlook.com>
2023-04-25 22:38:20 +0200

    FEAT: Add buttons readout and keyboard presses
    The base sketch has been modified to read the buttons (using the
    precalculated values ) and send the correct keyboard press.
    There are multiple commented Serial.println for debuggin purposes.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 
 
-| Button               | Resistance (ohm) | arduino value |
-| -------------------- | ---------------- | ------------- |
-| VOL -                | 55               | 915           |
-| VOL +                | 148              | 860           |
-| AUTO SCAN            | 301.3            | 705           |
-| PRESET               | 563              | 625           |
-| MODE                 | 1.041k           | 475           |
-| MUTE                 | 2.040K           | 406           |
-| When nothing pressed | 5.02K            | 176           |
+| Button               | Resistance (ohm) | arduino value | keyboard press (for openauto pro) |
+| -------------------- | ---------------- | ------------- | --------------------------------- |
+| VOL -                | 55               | 915           | F7                                |
+| VOL +                | 148              | 860           | F8                                |
+| AUTO SCAN            | 301.3            | 705           | N                                 |
+| PRESET               | 563              | 625           | V                                 |
+| MODE                 | 1.041k           | 475           | H                                 |
+| MUTE                 | 2.040K           | 406           | B                                 |
+| When nothing pressed | 5.02K            | 176           |                                   |
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "Keyboard.h"
 
 #define ANALOG_PIN A0
 
@@ -24,7 +25,8 @@ const int MUTE_DELAY = 150;
 
 void setup()
 {
-  Serial.begin(9600);
+  //Serial.begin(9600);
+  Keyboard.begin();
   // put your setup code here, to run once:
 }
 
@@ -35,33 +37,39 @@ void loop()
   delay(10);
   if (value >= VOL_DOWN * ((100.0 - STANDARD_DEVIATION) / 100.0) && value <= VOL_DOWN * ((100.0 + STANDARD_DEVIATION) / 100.0))
   {
-    Serial.println("VOL DOWN: " + String(value) + "/" + VOL_DOWN);
-
+    //Serial.println("VOL DOWN: " + String(value) + "/" + VOL_DOWN);
+    Keyboard.press(KEY_F7);
     delay(VOL_DOWN_DELAY);
   }
   else if (value >= VOL_UP * ((100.0 - STANDARD_DEVIATION) / 100.0) && value <= VOL_UP * ((100.0 + STANDARD_DEVIATION) / 100.0))
   {
-    Serial.println("VOL_UP: " + String(value) + "/" + VOL_UP);
+    //Serial.println("VOL_UP: " + String(value) + "/" + VOL_UP);
+    Keyboard.press(KEY_F8);
     delay(VOL_UP_DELAY);
   }
   else if (value >= UP * ((100.0 - STANDARD_DEVIATION) / 100.0) && value <= UP * ((100.0 + STANDARD_DEVIATION) / 100.0))
   {
-    Serial.println("UP: " + String(value) + "/" + UP);
+    //Serial.println("UP: " + String(value) + "/" + UP);
+    Keyboard.press(110); // ASCII for n
     delay(UP_DELAY);
   }
   else if (value >= DOWN * ((100.0 - STANDARD_DEVIATION) / 100.0) && value <= DOWN * ((100.0 + STANDARD_DEVIATION) / 100.0))
   {
-    Serial.println("DOWN: " + String(value) + "/" + DOWN);
+    //Serial.println("DOWN: " + String(value) + "/" + DOWN);
+    Keyboard.press(118); // ASCII for v
     delay(DOWN_DELAY);
   }
   else if (value >= MODE * ((100.0 - STANDARD_DEVIATION) / 100.0) && value <= MODE * ((100.0 + STANDARD_DEVIATION) / 100.0))
   {
-    Serial.println("MODE: " + String(value) + "/" + MODE);
+    //Serial.println("MODE: " + String(value) + "/" + MODE);
+    Keyboard.press(104); // ASCII for h
     delay(MODE_DELAY);
   }
   else if (value >= MUTE * ((100.0 - STANDARD_DEVIATION) / 100.0) && value <= MUTE * ((100.0 + STANDARD_DEVIATION) / 100.0))
   {
-    Serial.println("MUTE: " + String(value) + "/" + MUTE);
+    //Serial.println("MUTE: " + String(value) + "/" + MUTE);
+    Keyboard.press(98); // ASCII for b
     delay(MUTE_DELAY);
   }
+  Keyboard.releaseAll();
 }
